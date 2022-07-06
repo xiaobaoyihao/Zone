cf215595aee56e9bfd5acb04f93f4fb0668fdbb7
dingbaosheng <dingbaosheng@mockuai.com>
2022-07-06 16:39:44 +0800

    添加gradle task构建日志，编译分析

--- zone/src/main/groovy/com/zone/plugin/ZonePlugin.groovy
+++ zone/src/main/groovy/com/zone/plugin/ZonePlugin.groovy
@@ -1,13 +1,24 @@
 package com.zone.plugin
+
+
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class ZonePlugin implements Plugin<Project>{
+class ZonePlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
         project.afterEvaluate { Project pj ->
-            println("=====hello this is zone plugin ${pj.name}=====")
+            println("=====hello this is zone plugin1 ${pj.name}=====")
+
+            if (project.plugins.hasPlugin("com.android.application")) {
+                println("has apply plugin: com.android.application ")
+                pj.tasks.register("hello") {
+                    doLast {
+                        println("hello task do last")
+                    }
+                }
+            }
         }
     }
 }
