84513c5999cd4e4a6ff7e01bd0951140c85221a0
dingbaosheng <dingbaosheng@mockuai.com>
2022-07-06 18:14:47 +0800

    拦截编译java类任务

--- zone/src/main/groovy/com/zone/plugin/ZonePlugin.groovy
+++ zone/src/main/groovy/com/zone/plugin/ZonePlugin.groovy
@@ -1,8 +1,10 @@
 package com.zone.plugin
 
-
+import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.compile.JavaCompile
 
 class ZonePlugin implements Plugin<Project> {
 
@@ -19,6 +21,25 @@ class ZonePlugin implements Plugin<Project> {
                     }
                 }
             }
+            /**
+             * 对于class，
+             * 1. 如果是java，hook编译java文件任务（compileBetaDebugJavaWithJavac），以获取java文件路径
+             * 2. 对于是kotlin文件， hook（kaptGenerateStubsBetaDebugKotlin）任务，以获取kotlin源文件路径
+             */
+            project.gradle.taskGraph.afterTask(new Action<Task>() {
+                @Override
+                void execute(Task task) {
+                    if(task instanceof JavaCompile){
+                        /**
+                         * java编译任务
+                         */
+                        println("${task.project.name}:afterTask:${task.name}")
+                        task.inputs.files.forEach{
+                            println("input file:$it.absolutePath")
+                        }
+                    }
+                }
+            })
         }
     }
 }
